6568da594135def7c294a67897ea71d232c23c04
leosilvas-collab <leocladison@gmail.com>
2025-11-07 19:09:25 -0300

    Inclusão toogle menu no script.js e correção do menu hamburguer

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Adiciona um elemento de menu móvel ao clicar no ícone
+function toggleMenu() {
+    var links = document.getElementById("nav-links-mobile");
+    links.classList.toggle("ativo");
+}
